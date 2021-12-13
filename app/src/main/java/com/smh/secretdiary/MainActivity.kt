ef1e80f12558c5328e9f9e371e2f9e34874173f6
MinhyeokSun <s.minyeok@gmail.com>
2021-12-13 20:53:17 +0900

    4th Commit

--- app/src/main/java/com/smh/secretdiary/MainActivity.kt
+++ app/src/main/java/com/smh/secretdiary/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.smh.secretdiary
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -65,9 +66,7 @@ class MainActivity : AppCompatActivity() {
             val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
 
             if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
-                //패스워드 성공
-                // TODO 다이어리 페이지 열기
-                //startActivity()
+                startActivity(Intent(this, DiaryActivity::class.java))
             } else {
                 //패스워드 실패
                 showErrorAlertDialog()
